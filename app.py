8be9dbcf90ebc64eb1968d9f38e1d5501d6ed034
Natyren <bredis.georgy@yandex.ru>
2022-12-27 11:44:50 +0300

    First commit with parsing

---
+++ app.py
@@ -0,0 +1,50 @@
+from telethon import TelegramClient
+import os
+import json
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.types import ChannelParticipantsSearch
+from telethon.tl.types import (
+    PeerChannel
+)
+
+api_id = os.environ['TELEGRAM_API_ID']
+api_hash = os.environ['TELEGRAM_API_HASH']
+client = TelegramClient('anon', api_id, api_hash)
+
+async def pp():
+    catalog = await client.get_dialogs()
+    offset = 0
+    limit = 100
+    all_participants = {}
+    for dialog in catalog:
+        try:
+            if dialog.is_group:
+                print(dialog.name, dialog.id)
+                all_participants[dialog.name] = []
+                curr_participants = []
+                my_channel = await client.get_entity(dialog.id)
+
+                while True:
+                    participants = await client(GetParticipantsRequest(
+                        my_channel, ChannelParticipantsSearch(''), offset, limit,
+                        hash=0
+                    ))
+                    if not participants.users:
+                        break
+                    curr_participants.extend(participants.users)
+                    offset += len(participants.users)
+
+                for participant in curr_participants:
+                    all_participants[dialog.name].append(
+                        {"id": participant.id, "first_name": participant.first_name, "last_name": participant.last_name,
+                        "user": participant.username, "phone": participant.phone, "is_bot": participant.bot})
+
+        except Exception as e:
+            print(e)
+
+    with open('/Users/georgebredis/Data/TelegramNetworkData/data_v1.json', 'w') as outfile:
+        json.dump(all_participants, outfile)
+
+with client:
+    client.loop.run_until_complete(pp())
